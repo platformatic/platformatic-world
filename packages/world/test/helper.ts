da41b30460051c642ab661f40f984bf66743de37
marcopiraccini <marco.piraccini@gmail.com>
2026-03-09 22:34:54 +0100

    linting fixup
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- packages/world/test/helper.ts
+++ packages/world/test/helper.ts
@@ -1,8 +1,3 @@
-import { randomBytes } from 'node:crypto'
-
-// This helper is used for integration tests that require a running workflow service.
-// Start the service first: node --experimental-strip-types packages/workflow/src/server.ts
-
 export const SERVICE_URL = process.env.WF_SERVICE_URL || 'http://localhost:3042'
 
 export async function provisionApp (): Promise<{ appId: string; apiKey: string }> {
